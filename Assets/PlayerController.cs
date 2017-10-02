a076d3da095eef1107ab6c4071fa6763b9708382
Jacob Blanch <1006027@student.sae.edu.au>
2017-10-02 19:19:45 +1000

    Gotta sick new sound effect for squeezing pimples, also now you have to hold down the moust button to pop em

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,30 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public GameObject splat;
+    
+    public float damage;
+    AudioSource audioSource;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-		
-	}
+        audioSource = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1"))
         {
             RaycastHit hit;
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
-                if (hit.transform.gameObject.tag == "Pimple")
-                {
-                    Instantiate(splat, hit.transform.position, hit.transform.rotation);
-                    Destroy(hit.transform.gameObject);
-                }
+                    PimpleScript pimpleScript = hit.transform.gameObject.GetComponent<PimpleScript>();
 
+                    if (pimpleScript != null)
+                {
+                    pimpleScript.takeDamage(damage);
+                }
             }
         }
     }
--- Assets/Scripts/PimpleScript.cs
+++ Assets/Scripts/PimpleScript.cs
@@ -7,9 +7,15 @@ public class PimpleScript : MonoBehaviour {
     
     float timer;
     public float TimeToDie;
+    public float hp;
+    public GameObject splat;
+    AudioSource audioSource;
+    public AudioClip squeezeSound;
+    bool audioPlayed;
 
 	// Use this for initialization
 	void Start () {
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,4 +25,24 @@ public class PimpleScript : MonoBehaviour {
         else
             SceneManager.LoadScene("EndMenu");
 	}
+
+    public void takeDamage(float damage)
+    {
+        hp -= damage;
+        PlayAudio();
+        if (hp <= 0)
+        {
+            Instantiate(splat, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void PlayAudio()
+    {
+        if (!audioPlayed)
+        {
+            audioSource.PlayOneShot(squeezeSound);
+            audioPlayed = true;
+        }
+    }
 }
